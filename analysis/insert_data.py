4a67a0bc45a02c795071e54db574c73c360a0688
DESKTOP-984CM3C\gschoi <gyusik19@gmail.com>
2022-08-25 19:06:15 +0900

    backbone structure for database insertion

---
+++ analysis/insert_data.py
@@ -0,0 +1,6 @@
+import os
+import json
+
+# insert data into database from json files
+
+# if __name__=="__main__":
--- analysis/models.py
+++ analysis/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+
 # Create your models here.
+class Video(models.Model):
+    title = models.CharField(max_length=50)
+    link = models.CharField(max_length=20)
+
+
+class Content(models.Model):
+    video = models.ForeignKey(Video, on_delete=models.CASCADE)
+    timeStart = models.IntegerField()
+    timeEnd = models.IntegerField()
+    content = models.TextField()
